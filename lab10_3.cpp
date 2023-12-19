268f1055bc9cb7b0f51932a33487284b12823de8
temty180447 <tgtv2547@gmail.com>
2023-12-19 17:27:45 +0700

    lab3

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,23 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <string>
+#include <cstdlib>
+#include <fstream>
+using namespace std;
+
+int main(){
+    double nub = 0, miw = 0, stdy;
+    string x;
+    ifstream source("score.txt");
+    while (getline(source,x)){
+        miw += atof(x.c_str());
+        stdy += pow(atof(x.c_str()),2);
+        nub++;
+    }
+    double Mean = miw/nub;
+    cout << "Number of data = " << nub << endl;
+    cout << setprecision(3);
+    cout << "Mean = " << Mean << endl;
+    cout << "Standard deviation = " << sqrt((stdy/nub)-pow(Mean,2));
+}
